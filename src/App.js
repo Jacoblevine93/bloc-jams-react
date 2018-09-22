a694c74d6ade814611a75bada5960f5c9962c088
Jacob Levine <Jacob.levine93@protonmail.com>
2018-09-22 10:53:03 -0700

    Almost Done With Styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Library from './components/Library';
 import Album from './components/Album';
 import { Helmet } from "react-helmet";
 
+
+
 class App extends Component {
   render() {
     return (
@@ -13,29 +15,28 @@ class App extends Component {
       <Helmet>
       <style>{'body { background-color: #181818; }'}</style>
       </Helmet>
-        <header>
-        <nav class="navbar navbar-expand-md navbar-dark justify-content-center">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <Link class="nav-link" to="/">Home</Link>
+      <header>
+        <nav className="navbar navbar-expand-md navbar-dark justify-content-center">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        <div className="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to='/library'>Library</Link>
+            <li className="nav-item">
+              <Link className="nav-link" to='/library'>Library</Link>
             </li>
           </ul>
         </div>
-      </nav>
-        </header>
-        <main>
+        </nav>
+      </header>
+      <main>
         <Route exact path="/" component={Landing} />
         <Route path="/library" component={Library} />
         <Route path="/album/:slug" component={Album} />
-        </main>
+      </main>
       </div>
     );
   }
